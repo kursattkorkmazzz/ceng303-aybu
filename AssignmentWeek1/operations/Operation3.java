2d62e66d3272e090cfa6c01ccab5d51ad96e60d8
mehdiyargin <94059695+mehdiyargin@users.noreply.github.com>
2023-10-13 14:42:46 +0300

    Update Operation3.java
    Sen çok terbiyesizsin

--- AssignmentWeek1/operations/Operation3.java
+++ AssignmentWeek1/operations/Operation3.java
@@ -1,27 +1,19 @@
-package operations;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 
 public class Operation3 {
-	private int arr[];
-	private int counter;
-	private int arraysize;
 	private String filename;
 	private long start;
 	private long stop;
 	private long time;
 	private int index1;
 	private int index2;
-	
+	DynamicArray dy = new DynamicArray();
 	//Constructor for the DynamicArray with index1 and index2.
 	public Operation3(String filename, int index1, int index2) {
 		this.filename = filename;
-		arr = new int[1];
-		arraysize =1;
-		counter = 0;
 		this.index1 = index1;
 		this.index2 = index2;
 		readFromFile();
@@ -30,9 +22,6 @@ public class Operation3 {
 	//Second constructor for just index1.
 	public Operation3(String filename, int index1) {
 		this.filename = filename;
-		arr = new int[1];
-		arraysize =1;
-		counter = 0;
 		this.index1 = index1;
 		
 	}
@@ -43,7 +32,7 @@ public class Operation3 {
 			File file = new File(this.filename);
 			Scanner reader = new Scanner(file);
 			while(reader.hasNext()) {
-			addItem(reader.nextInt());	
+			dy.addItem(reader.nextInt());	
 			}
 			
 			return true;
@@ -54,46 +43,6 @@ public class Operation3 {
 		return false;
 	}
 	
-	// If we do not have enough size this method doubling size
-	public void growSize() {
-		int temp[] = null;
-		temp = new int[arraysize * 2];
-		
-		for(int i=0; i<arraysize; i++) temp[i] = arr[i];
-		
-		arr = temp;
-		arraysize *= 2;
-	}
-	
-	//This method is used for adding item to dynamic array 
-	public void addItem(int item) {
-		if(counter == arraysize) growSize();
-		
-		arr[counter] = item;
-		counter++;
-	}
-
-	//This method is used for adding item to first index
-	public void insertFirst(int item) {
-		if(counter == arraysize) growSize();
-		
-		for(int i = counter; 0<i; i--) arr[i] = arr[i-1];
-		arr[0] = item;
-		counter++;
-	}
-	
-	//This method is used for adding item to last index
-	public void insertLast(int item) {
-			if(counter == arraysize) growSize();
-		
-			arr[counter] = item;
-			counter++;
-	}
-	
-	//This method is used for reading an item from index
-	public int readIntFromIndex(int index) {
-		return arr[index];
-	}
 	public void printStatus() {
 		this.start = System.currentTimeMillis();
 		readFromFile();
@@ -102,19 +51,19 @@ public class Operation3 {
 		System.out.println("3a) The integer dynamic array is built in "+time+" milliseconds.");
 		
 		this.start = System.currentTimeMillis();
-		insertFirst(355);
+		dy.insertFirst(355);
 		this.stop = System.currentTimeMillis();
 		this.time = this.stop - this.start;
 		System.out.println("3b) An integer is inserted into the first index of the integer array in "+time+" milliseconds.");
 		
 		this.start = System.currentTimeMillis();
-		insertLast(9910);
+		dy.insertFirst(9910);
 		this.stop = System.currentTimeMillis();
 		this.time = this.stop - this.start;
 		System.out.println("3c) An integer is inserted into the last index of the integer array in "+time+" milliseconds.");
 		
 		this.start = System.currentTimeMillis();
-		int item = readIntFromIndex(index1);
+		int item = dy.readIntFromIndex(index1);
 		this.stop = System.currentTimeMillis();
 		this.time = this.stop - this.start;
 		System.out.println("3d) An integer, which is “"+item+"”, is read from the index "+index1+" of the integer \n" +
@@ -122,14 +71,12 @@ public class Operation3 {
 		
 		if(index2 != 0) {
 			this.start = System.currentTimeMillis();
-			int item2 = readIntFromIndex(index2);
+			int item2 = dy.readIntFromIndex(index2);
 			this.stop = System.currentTimeMillis();
 			this.time = this.stop - this.start;
 			System.out.println("3e) An integer, which is “"+item2+"”, is read from the index "+index2+" of the integer \n" +
 	                "array in "+time+" milliseconds.");
-		}
-		
+		}		
 	}
 
-	
 }
